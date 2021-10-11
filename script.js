128ff59e2c5445f93ed265a7a6a2a30617f82b11
Josh <joshuawen97@gmail.com>
2021-10-11 16:20:32 +0700

    destructing and arrows

--- script.js
+++ script.js
@@ -12,47 +12,177 @@ myObj['b'] = 'value'; //b: 'value
 
 let myNumber = 3;
 console.log({ myNumber }); //CL variable name and value (myNumber : 3)
-// EXERCISE SHORTHAND
-// const name = 'Parrot';
-// const colors = ['Red', 'Green', 'Blue', 'Yellow'];
 
-// const bird = {};
-// bird.name = name;
-// bird.colors = colors;
+function objectShorthand() {
+	//
+	// EXERCISE SHORTHAND
+	//
+	const name = 'Parrot';
+	const colors = ['Red', 'Green', 'Blue', 'Yellow'];
+	const talons = true;
 
-// console.log(bird);
+	const bird = {
+		name,
+		colors,
+		talons,
+	};
+	console.log(bird);
+	//
+	const capital = 'Hanoi';
+	const continent = 'Asia';
+
+	const vietnam = {
+		capital,
+		continent,
+	};
+	console.log(vietnam);
+	//
+	const wheelsCount = 4;
+	const doorsCount = 4;
+	const color = 'black';
+
+	const car = {
+		wheelsCount,
+		doorsCount,
+		color,
+	};
+	console.log(car);
+}
+function restSpread() {
+	// OBJECT DESRUCTURING/////
+	const myObject = { x: 1, y: 2, myArray: ['a', 'b', 'c'] };
+
+	let { x, y, myArray } = myObject; // Acts the same as the code below
+	// let x = myObject.x;
+	// let y = myObject['y'];
+	// let myArray = myObject.myArray;
+	console.log(x, y, myArray);
+	console.log(myObject);
+	let [one, two] = myArray; //targets the position of the elements
+	console.log(one, two); //Outputs a, b
+	//////////////////////////////////////////////////////////////////////
+	// REST AND SPREAD
+	const state = { counter: 1, list: ['a', 'b'] };
+
+	// rest destructuring
+	const { list, ...rest } = state;
+
+	console.log(rest); // output: { counter: 1 }
+	console.log(list); // output: ["a", "b"]
+	//
+	const num = [-1, 5, 11, 3];
+	// let [a, b, c, d] = num;
+	console.log(Math.max(...num));
+	// if the ... is on the 'left of operator' it is REST.  else it's SPREAD
+
+	// let [a, ...haha] = arr; //...haha is REST
+	// let arr = [...haha]; //...haha is SPREAD
+	//////////////////////////////////////////////////////////////////////
+	// const x = ['a', 'b'];
+	// 	const y = ['c'];
+	// 	const z = ['d', 'e'];
+	// 	const add = [1, 2, 3];
+	// 	const combine = [...x, ...y, ...z]; // ["a", "b", "c", "d", "e"]
+	// 	console.log(combine);
+	// 	combine.push(...add); //adds(spreads) 1,2,3 into the array
+	// 	console.log(combine);
+}
 //
-const name = 'Parrot';
-const colors = ['Red', 'Green', 'Blue', 'Yellow'];
-const talons = true;
+function getAverage(obj) {
+	// console.log(`getAverage`, obj);
+	// console.log(`Value of x: `, obj.x);
+	// console.log(`Value of y: `, obj.y);
+	// console.log(`Value of z: `, obj.z);
+	// let x = obj.x;
+	// let y = obj.y;
+	// let z = obj.z;
 
-const bird = {
-	name: name,
-	colors: colors,
-	// CHANGE ME
-};
-bird.talons = talons;
+	let { x, y, z } = obj;
+	return Math.floor((x + y + z) / 3.0); // CHANGE ME
+}
 
-console.log(bird);
+console.log(getAverage({ x: 3.6, y: 7.8, z: 4.3 }));
 //
-const capital = 'Hanoi';
-const continent = 'Asia';
-
-const vietnam = {
-	// CHANGE ME
+function getAddress(city, country, address) {
+	console.log({ city, country, address });
+	let { street } = address;
+	return city === 'HCMC' && country === 'Vietnam' && street === 'Ton Dan'; // CHANGE ME
+}
+let data = {
+	city: 'HCMC',
+	country: 'Vietnam',
+	address: {
+		number: 12,
+		street: 'Ton Dan',
+		district: '4',
+	},
 };
-vietnam.capital = capital;
-vietnam.continent = continent;
-console.log(vietnam);
+// console.log(getAddress(data));
 //
-const wheelsCount = 4;
-const doorsCount = 4;
-const color = 'black';
+function objectifyElements(arr) {
+	let { first, second, third } = arr;
+	return { first, second, third }; // CHANGE ME
+}
 
-const car = {
-	// CHANGE ME
-};
-car.wheelsCount = wheelsCount;
-car.doorsCount = doorsCount;
-car.color = color;
-console.log(car);
+console.log(objectifyElements([0, 1, 2, 3, 4, 5, 6, 7, 8]).fourth === 4);
+//
+function getFoodItems() {
+	const food = [
+		['carrots', 'beans', 'peas', 'lettuce'],
+		['apples', 'mangos', 'oranges'],
+		['cookies', 'cake', 'pizza', 'chocolate'],
+	];
+
+	const carrots = food[0][0];
+	const cookies = food[2][0];
+	const mangos = food[1][2]; // CHANGE ME
+
+	return { carrots, cookies, mangos }; // CHANGE ME
+}
+
+console.log(getFoodItems().mangos === 'mangos');
+// ARROW FUNCTIONS
+function multiArgument() {
+	// CHANGE BELOW
+
+	const divide = (a, b) => a / b;
+	return divide(40, 10);
+}
+console.log(multiArgument());
+//
+function spreadWithArrow() {
+	// CHANGE BELOW
+	const asArray = (...args) => args;
+	return asArray(1, 2, 3, 4);
+}
+console.log(spreadWithArrow());
+//
+function withObject() {
+	// CHANGE BELOW
+	const getObject = (favoriteCandy) => favoriteCandy;
+	return getObject('twix');
+}
+console.log(withObject());
+//
+function withMultiLineExpression() {
+	// CHANGE BELOW
+	const getString = (name) =>
+		`
+		  Hello there ${name}
+		  How are you doing today?
+		`;
+	return getString('Ryan');
+}
+console.log(withMultiLineExpression());
+//
+function curryAdd() {
+	// CHANGE BELOW
+	const curryAddition = (a) =>
+		function (b) {
+			return function (c) {
+				return a + b + c;
+			};
+		};
+	return curryAddition(9)(3)(5);
+}
+console.log(curryAdd());
